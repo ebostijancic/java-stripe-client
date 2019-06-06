c6254290f96505767dd22739fa0fa02b0d473ff4
Emil Bostijancic <emil.bostijancic@gmail.com>
2019-06-06 23:00:10 +0200

    initial commit.

---
+++ src/main/java/com/github/ebostijancic/stripe/JavaStripeClientApplication.java
@@ -0,0 +1,13 @@
+package com.github.ebostijancic.stripe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStripeClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaStripeClientApplication.class, args);
+	}
+
+}
